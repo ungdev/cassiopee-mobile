fbc6571058c4a01bd8dea983134aed5ceacd2fc1
quentltl <quentin.letellier@utt.fr>
2021-04-02 13:39:25 +0200

    new bundle social

--- src/Bundles/Social/Screen/SocialScreen.js
+++ src/Bundles/Social/Screen/SocialScreen.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import Header from '../../../components/Header'
-import TabNavigatorSocial from '../TabNavigatorSocial'
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView, View, StyleSheet } from 'react-native'
 import i18n from '../../../translate/index'
+import { Icon } from 'react-native-elements'
+import { Linking } from 'react-native'
+import Device from 'react-native-device-detection'
 
 class SocialScreen extends Component {
   render() {
@@ -15,11 +17,85 @@ class SocialScreen extends Component {
           }}
         >
           <Header bigtitle={i18n.t('menu_social')} />
-          <TabNavigatorSocial />
+          <View style={styles.mainContainer}>
+            <View style={styles.column}>
+              <View style={styles.row}>
+                <Icon
+                  name="facebook"
+                  type="font-awesome-5"
+                  color="#1977f3"
+                  size={Device.isTablet ? 250 : 150}
+                  iconStyle={styles.icon}
+                  onPress={() =>
+                    Linking.openURL('https://www.facebook.com/gala.utt')
+                  }
+                />
+                <Icon
+                  name="instagram"
+                  type="font-awesome"
+                  color="whitesmoke"
+                  size={Device.isTablet ? 250 : 150}
+                  iconStyle={styles.icon}
+                  onPress={() =>
+                    Linking.openURL(
+                      'https://www.instagram.com/cassiopee_galautt/'
+                    )
+                  }
+                />
+              </View>
+              <View style={styles.row}>
+                <Icon
+                  name="twitter"
+                  type="font-awesome"
+                  color="#1da1f2"
+                  size={Device.isTablet ? 250 : 150}
+                  iconStyle={styles.icon}
+                  onPress={() => Linking.openURL('https://twitter.com/GalaUTT')}
+                />
+                <Icon
+                  name="youtube-play"
+                  type="font-awesome"
+                  color="#c00"
+                  size={Device.isTablet ? 250 : 140}
+                  iconStyle={styles.icon}
+                  onPress={() =>
+                    Linking.openURL(
+                      'https://www.youtube.com/channel/UCLprjLc5CJMNUjSolrTyu4g'
+                    )
+                  }
+                />
+              </View>
+            </View>
+          </View>
         </SafeAreaView>
       </React.Fragment>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginBottom: 20,
+    marginTop: 20,
+    width: '100%',
+    padding: 10,
+  },
+  column: {
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  icon: {
+    margin: 15,
+  },
+})
 
 export default SocialScreen
